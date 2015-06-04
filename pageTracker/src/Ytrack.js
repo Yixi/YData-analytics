9fbfa57e39ae2004869d098ae8961719129d8e2a
Yixi <lyh1112@gmail.com>
2015-06-04 18:03:53 +0800

    ignore

---
+++ pageTracker/src/Ytrack.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Yixi on 6/4/15.
+ */
+
+(function(){
+
+    var util = {
+        test:'abc'
+    };
+
+    var womendoushidad = function(){ util.test = "1" };
+
+    var nidamfndasdafd = function(){womendoushidad()};
+
+
+    nidamfndasdafd();
+
+
+
+})(window);
